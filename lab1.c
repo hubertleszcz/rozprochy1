da33960c49f647b15e3d18ad054e62c386da73b9
leszcz <44520625+hubertleszcz@users.noreply.github.com>
2024-03-05 21:31:51 +0100

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -1,73 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
 
 
-struct listNode{
+struct listNode {
     char* data;
     struct listNode* prev;
     struct listNode* next;
 };
 
+struct listNode* insert(struct listNode* head, char* temporaryBuffer) {
+    struct listNode* newNode = head->next;
 
-
-
-void deleteNode(struct listNode* head, int deletedNode){
-    struct listNode* current = head;
-    for(int i=0;i<deletedNode;i++){
-        current = current->next;
-        if(current==NULL) return;
-    }
-    free(current->data);
-    current->prev->next = current->next;
-    current->next->prev = current->prev;
-    current->next = NULL;
-    current->prev = NULL;
-    free(current);
-}
-
-
-void freeMemory(struct listNode* head) {
-    struct listNode* curr = head->next;
-    struct listNode* next;
-
-    while (curr != NULL) {
-        next = curr->next; 
-        free(curr->data); 
-        free(curr); 
-        curr = next;
-    }
-    head->next = NULL;
-    free(head);
-}
-void printOut(struct listNode* head){
-    struct listNode* current = head;
-    while(current != NULL){
-        printf("%s", current->data);
-        current = current->next;
-    }
-}
-
-struct listNode* insert(struct listNode* head, char* temporaryBuffer){
-	struct listNode* newNode = head->next;
-    
-    if(newNode == NULL){
+    if (newNode == NULL) {
         newNode = (struct listNode*)malloc(sizeof(struct listNode));
-        newNode->data = (char*)malloc(strlen(temporaryBuffer));
+        newNode->data = (char*)malloc((strlen(temporaryBuffer) + 1) * sizeof(char));
         strcpy(newNode->data, temporaryBuffer);
         newNode->prev = head;
         newNode->next = NULL;
     }
-    else{
-        if(strcmp(newNode->data,temporaryBuffer)>0){
+    else {
+        if (strcmp(newNode->data, temporaryBuffer) > 0) {
             newNode->next = insert(newNode, temporaryBuffer);
-        }        
-        else{
-            
+        }
+        else {
             newNode->prev = (struct listNode*)malloc(sizeof(struct listNode));
             newNode = newNode->prev;
-            newNode->data = (char*)malloc(strlen(temporaryBuffer));
+            newNode->data = (char*)malloc((strlen(temporaryBuffer) + 1) * sizeof(char));
             strcpy(newNode->data, temporaryBuffer);
             newNode->prev = head;
             newNode->next = head->next;
@@ -78,35 +40,82 @@ struct listNode* insert(struct listNode* head, char* temporaryBuffer){
     return newNode;
 }
 
-int main(){
+void deleteNode(struct listNode* head, int deletedNode) {
+    struct listNode* current = head;
+    for (int i = 0; i < deletedNode; i++) {
+        current = current->next;
+        if (current == NULL) return;
+    }
+    free(current->data);
+    current->prev->next = current->next;
+    current->next->prev = current->prev;
+    current->next = NULL;
+    current->prev = NULL;
+    free(current);
+}
+
+
+
+
+
+
+void freeMemory(struct listNode* head) {
+    struct listNode* nextNode;
+    struct listNode* current = head->next;
+    while (current != NULL) {
+        nextNode = current->next;
+        free(current->data);
+        free(current);
+        current = nextNode;
+    }
+
+    free(head);
+}
+
+void printOut(struct listNode* head) {
+    struct listNode* current = head;
+    while (current != NULL) {
+        printf("%s\n", current->data);
+        current = current->next;
+    }
+}
+
+
+int main() {
+    //lista 2kierunkowa
+    //napisy
+    //jak dodajesz to alfabetycznie malejaco    
+    // insert i delete
+    //max 100 znakow
     int deletedNode, ongoing = 1;
     size_t stringLength, newLength;
-	int choice;
+    int choice;
     struct listNode* head = (struct listNode*)malloc(sizeof(struct listNode));
     head->next = NULL;
     head->prev = NULL;
-    while(ongoing){
+    while (ongoing) {
         scanf("%d", &choice);
-        switch(choice){
-            case 1:
-                stringLength = 100;
-	            char* temporaryBuffer = (char*)malloc(stringLength*sizeof(char));
-                newLength = getline(&temporaryBuffer, &stringLength, stdin);
-                head->next = insert(head, temporaryBuffer);
-                free(temporaryBuffer);
-                break;
-            case 2:
-                scanf("%d",&deletedNode);
-                deleteNode(head,deletedNode);
-                break;
-            case 3:
-                printOut(head->next);
-                break;
-            case 0:
-                ongoing = 0;
-                break;
+        switch (choice) {
+        case 1:
+            stringLength = 100;
+            char* temporaryBuffer = (char*)malloc(stringLength * sizeof(char));
+            newLength = getline(&temporaryBuffer, &stringLength, stdin);
+            head->next = insert(head, temporaryBuffer);
+            free(temporaryBuffer);
+            break;
+        case 2:
+            scanf("%d", &deletedNode);
+            deleteNode(head, deletedNode);
+            break;
+        case 3:
+            printOut(head->next);
+            break;
+        case 0:
+            ongoing = 0;
+            break;
         }
     }
+
     freeMemory(head);
     return 0;
 }
